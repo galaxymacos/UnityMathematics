d8068e938a963e85da9bb7e07a1114388da10fb7
Xun Ruan <galaxymacos@gmail.com>
2020-06-18 02:32:44 -0400

    BitBoard

---
+++ Assets/BitBoards.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitBoards : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    long SetCellState(long bitBoard, int row, int col)
+    {
+        long newBit = 1L << (row * 8 + col);
+
+        return bitBoard |= newBit;
+    }
+
+    bool GetCellState(long bitBoard, int row, int col)
+    {
+        long mask = 1L << (row * 8 + col);
+        return (mask & bitBoard) != 0;
+    }
+}
