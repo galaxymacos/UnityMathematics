237d848150c71de0458fc45e1ee87b97347bf589
Xun Ruan <galaxymacos@gmail.com>
2020-06-18 22:23:59 -0400

    DIY Width and Height of Graph

--- Assets/DrawGraph.cs
+++ Assets/DrawGraph.cs
@@ -1,31 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class DrawGraph : MonoBehaviour
 {
     public int size = 20;
+    public int maxWidth;
+    public int maxHeight;
 
-    private Coo2D startPointXAxis = new Coo2D(160, 0);
-    private Coo2D endPointXAxis = new Coo2D(-160, 0);
-    
-    private Coo2D startPointYAxis = new Coo2D(0, 100);
-    private Coo2D endPointYAxis = new Coo2D(0, -100);
-    
-    
-    
-    
+    private Coo2D startPointXAxis;
+    private Coo2D endPointXAxis;
+
+    private Coo2D startPointYAxis;
+    private Coo2D endPointYAxis;
+
+    private void Awake()
+    {
+        startPointXAxis = new Coo2D(maxWidth, 0);
+        endPointXAxis = new Coo2D(-maxWidth, 0);
+        startPointYAxis = new Coo2D(0, maxHeight);
+        endPointYAxis = new Coo2D(0, -maxHeight);
+    }
+
+
     private void Start()
     {
-        DrawUtility.DrawLine(new Coo2D(0,-100), new Coo2D(0,100),1f, Color.green);
-        DrawUtility.DrawLine(new Coo2D(-160,0), new Coo2D(160,0),1f, Color.red);
+        DrawUtility.DrawLine(new Coo2D(0,-maxHeight), new Coo2D(0,maxHeight),1f, Color.green);
+        DrawUtility.DrawLine(new Coo2D(-maxWidth,0), new Coo2D(maxWidth,0),1f, Color.red);
 
-        for (int x = -160; x <= 160; x+=size)
+        int xInterval = (int) (maxWidth / (float) size);
+
+        for (int x = -xInterval*size; x <= xInterval*size; x+=size)
         {
-            DrawUtility.DrawLine(new Coo2D(x, -100), new Coo2D(x, 100), 0.5f, Color.white);
+            DrawUtility.DrawLine(new Coo2D(x, -maxHeight), new Coo2D(x, maxHeight), 0.5f, Color.white);
         }
 
-        for (int y = -100; y <= 100; y+=size)
+        int yInterval = (int) (maxHeight / (float) size);
+
+        for (int y = -yInterval*size; y <= yInterval*size; y+=size)
         {
-            DrawUtility.DrawLine(new Coo2D(-160, y), new Coo2D(160, y), 0.5f, Color.white);
+            DrawUtility.DrawLine(new Coo2D(-maxWidth, y), new Coo2D(maxWidth, y), 0.5f, Color.white);
         }
     }
 }
